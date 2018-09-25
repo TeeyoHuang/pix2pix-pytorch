4c155d167333af0989a92c815df4b9f2450a4058
TeeyoHuang <teeyohuang@163.com>
2018-09-25 23:03:40 +0800

    Update models.py

--- models.py
+++ models.py
@@ -110,34 +110,6 @@ class GeneratorUNet(nn.Module):
 #        Discriminator
 ##############################
 
-class Discriminator(nn.Module):
-    def __init__(self, args):
-        super(Discriminator, self).__init__()
-        self.in_channels = args.in_channels
-        def discriminator_block(in_filters, out_filters, normalization=True):
-            """Returns downsampling layers of each discriminator block"""
-            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
-            if normalization:
-                layers.append(nn.InstanceNorm2d(out_filters))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
-
-        self.model = nn.Sequential(
-            *discriminator_block(self.in_channels*2, 64, normalization=False),
-            *discriminator_block(64, 128),
-            *discriminator_block(128, 256),
-            *discriminator_block(256, 512),
-            nn.ZeroPad2d((1, 0, 1, 0)),
-            nn.Conv2d(512, 1, 4, padding=1, bias=False),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, img_A, img_B):
-        # Concatenate image and condition image by channels to produce input
-        img_input = torch.cat((img_A, img_B), 1)
-
-        return self.model(img_input)
-
 
 class Discriminator_n_layers(nn.Module):
     def __init__(self, args ):
@@ -204,10 +176,7 @@ class Discriminator_n_layers(nn.Module):
 ####################################################
 def Create_nets(args):
     generator = GeneratorUNet()
-    if (args.n_D_layers==0):
-        discriminator = Discriminator(args)
-    else:
-        discriminator = Discriminator_n_layers(args)
+    discriminator = Discriminator_n_layers(args)
 
     if torch.cuda.is_available():
         generator = generator.cuda()
